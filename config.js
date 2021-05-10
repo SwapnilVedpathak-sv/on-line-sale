8a04f512272aac24e804f9576df0eff5642eb3ad
SwapnilVedpathak-sv <swapnilvedpathak.sv@gmail.com>
2021-05-11 00:49:36 +0530

    uploading new project

---
+++ config.js
@@ -0,0 +1,9 @@
+//All the configurations required like portNumber, Database Connection URl etc are stored to provide a low level encapsulation
+
+module.exports = {
+  database:
+    'mongodb+srv://SwapnilVedpathak:Laxman9884@studentdata.fiifh.mongodb.net/furnitureMart?retryWrites=true&w=majority',
+  port: 3030,
+  secret: 'RamtejaRepaka2012312321'
+};
+
